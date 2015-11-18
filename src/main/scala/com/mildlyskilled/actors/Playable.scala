8bd893914ad017a7e174f2e2fbbc338e7522abd8
Kwabena Aning <kwabena.aning@worldfirst.com>
2015-11-18 13:06:06 +0000

    Stopping work on using FSM now

--- src/main/scala/com/mildlyskilled/actors/Playable.scala
+++ src/main/scala/com/mildlyskilled/actors/Playable.scala
@@ -13,50 +13,59 @@ trait Playable extends Actor with ActorLogging with FSM[State, Data] {
   implicit val insults = knownInsults
   implicit val comebacks = knownComebacks
 
-  startWith(Insulting, Uninitialised)
+  startWith(Insulting, MyGameData(0, knownInsults, knownComebacks, None))
 
   when(Insulting) {
 
-    case Event(Select(x), Uninitialised) => {
-
-      insults foreach { i => println(s"[${i.id}] ${i.content}") }
+    case Event(Select(x), insultsSection @ MyGameData(_, playerInsults, _)) =>
 
       insults find { i => i.id == x } match {
         case Some(i) => {
           sender() ! InsultMessage(i)
-          stay using MyGameData(0, i :: Nil, Nil)
+          stay()
         }
-        case None => sender() ! Info("You do not know this insult")
-      }
-      stay()
-    }
-
-
-    case Event(InsultMessage(x), i@MyGameData(_, collectedInsults, _)) => {
-      comebacks find (_.id == x.id) match {
         case None => {
-          sender() ! ConcedeRound
-          goto(Insulted) using i.copy(insults = x :: collectedInsults)
-        }
-        case Some(c) => {
-          sender() ! ComebackMessage(c)
-          goto(Insulted) using i
+          sender() ! Info("You do not know this insult")
+          stay()
         }
       }
-    }
 
-    case Event(Leave, _) => {
+    case Event(InsultMessage(x), i@MyGameData(score, collectedInsults, collectedComebacks, currentInsult)) =>
+      comebacks find (_.id == x.id) match {
+        case None =>
+          sender() ! ConcedeRound
+          goto(Insulted) using i.copy(insults = x::collectedInsults, currentInsult = x)
+
+        case Some(c) =>
+          goto(Insulted) using i
+      }
+
+
+    case Event(Leave, _) =>
       log.warning("Shutting down")
       stop(FSM.Shutdown)
-    }
 
-    case Event(e, s) => {
-      log.warning("received unhandled request {} in State {}/{}", e, stateName, s)
+
+    case Event(e, s) =>
+      log.warning("Received unknown message {} in State {}/{}", e, stateName, s)
       stay()
-    }
+
 
   }
 
+
+  when(Insulted) {
+    case Event(Select(c), Uninitialised) =>
+      comebacks find {comeback => comeback.id == c } match {
+        case Some(comeback) =>
+          sender() ! ComebackMessage(comeback)
+          stay using MyGameData(0, Nil, comeback :: Nil, None)
+
+        case None =>
+          sender() ! ConcedeRound
+          stay()
+      }
+  }
 
   onTransition {
     case _ -> Insulting => insults foreach { i => println(s"[${i.id}] ${i.content}") }
--- src/main/scala/com/mildlyskilled/messages/Protocol.scala
+++ src/main/scala/com/mildlyskilled/messages/Protocol.scala
@@ -4,38 +4,72 @@ import akka.actor.ActorRef
 import com.mildlyskilled.models.{Comeback, Insult, Entry}
 
 object Protocol {
+
   sealed trait Message
+
   case class Info(msg: String) extends Message
+
   case class InsultMessage(insult: Insult) extends Message
+
   case class ComebackMessage(comeback: Comeback) extends Message
+
   case class KnownInsults(insults: List[Insult]) extends Message
+
   case class KnownComebacks(insults: List[Comeback]) extends Message
+
   case class Select(id: Int) extends Message
+
   case class SelectInsult(id: Int) extends Message
+
   case class Register(player: ActorRef) extends Message
+
   case class Unregister(player: ActorRef) extends Message
+
   case class ResetPlayerScore(player: ActorRef) extends Message
+
   case object Turn extends Message
+
   case object Registered extends Message
+
   case object GoAway extends Message
+
   case object WaitingForEngagement extends Message
+
   case object ReadyToEngage extends Message
+
   case object GetInsults extends Message
+
   case object GetComebacks extends Message
+
   case object ConcedeRound extends Message
+
   case object ConcedeGame extends Message
+
   case object Initialise extends Message
+
   case object AnotherGame extends Message
+
   case object ListPlayers extends Message
+
   case object GetScores extends Message
+
   case object Leave extends Message
 
   sealed trait State
+
   case object Insulting extends State
+
   case object Insulted extends State
 
   sealed trait Data
+
   case object Uninitialised extends Data
-  final case class MyGameData(score: Int, insults: List[Insult], comebacks:List[Comeback]) extends Data
-  final case class CurrentInsult(insult: Insult) extends Data
+
+  final case class MyGameData(score: Int,
+                              insults: List[Insult],
+                              comebacks: List[Comeback],
+                              currentInsult: Option[Insult]) extends Data {
+    def updateInsults(i: Insult) = this.copy(insults = i :: insults)
+  }
+
 }
